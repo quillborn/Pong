86b0f5ee5942ace466508d7cf8c5589a131b6be8
Carter Terry Jr <cptj@kaavik.com>
2024-12-18 23:36:24 -0500

    V 1.0 Basic's completed. Game is functional, but prehaps not optomized.
    -created scoreboard
    -added functionality for bouncing off bounds, updating score, and increasing ball speed.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.tracer(0)
@@ -13,6 +14,8 @@ screen.title("Pong")
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball((0,0))
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up,"Up")
@@ -23,8 +26,26 @@ screen.onkeypress(l_paddle.go_down,"s")
 game_on = True
 
 while game_on:
-    time.sleep(0.1)
     screen.update()
     ball.move()
+    time.sleep(ball.move_speed)
+
+    #detect wall collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detec collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 300 or ball.distance(l_paddle) < 50 and ball.xcor() < -300:
+        ball.bounce_x()
+
+    #detect R paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #detect L paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -5,15 +5,15 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid=10,stretch_len=1)
+        self.shapesize(stretch_wid=8,stretch_len=1)
 
         self.penup()
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
+        new_y = self.ycor() + 30
         self.goto(self.xcor(),new_y)
 
     def go_down(self):
-        new_y = self.ycor() - 20
+        new_y = self.ycor() - 30
         self.goto(self.xcor(),new_y)
